9c2f062ea819df982cb5b54f41fb74b42cf29192
우수 <woosu91@gmail.com>
2026-03-02 10:48:29 +0900

    ▶️ 코드 실행: [level 0] Title: 잘라서 배열로 저장하기, Time: ., Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/20260422_095603_run_잘라서 배열로 저장하기.py
@@ -0,0 +1,22 @@
+def solution(my_str, n):
+    leng = int(len(my_str))
+    nn = int(n)
+    x = leng // nn
+    answer = []
+    
+    for i in range(1,x+1):
+        answer.append(my_str[(i-1)*nn:i*nn])
+    if x != 0:
+        answer.append(my_str[x*nn:leng+1])
+    
+    return answer
+
+# TypeError: unsupported operand type(s) for //: 'int' and 'str'
+# def solution(my_str, n):
+#     leng = len(my_str)
+#     x = leng // str(n)
+#     answer = []
+#     for i in range(1,x):
+#         answer.append(my_str[0,i*int(n)+1])
+#     answer.append(my_str[x*int(n)+1:leng+1]) 
+#     return answer
