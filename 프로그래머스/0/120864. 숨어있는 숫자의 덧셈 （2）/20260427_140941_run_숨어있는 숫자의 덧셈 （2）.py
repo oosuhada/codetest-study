86380900ba6ea3cdd86fa98aeb8e960fb563183b
우수 <woosu91@gmail.com>
2026-03-10 19:56:38 +0900

    ▶️ 코드 실행: [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/20260427_140941_run_숨어있는 숫자의 덧셈 （2）.py
@@ -0,0 +1,10 @@
+def solution(my_string):
+    my_list = []
+    for c in my_string:
+        if c.isdigit:
+            my_list.append(c)
+        else:
+            my_list.append(my_string.replace('',c))
+            
+    answer = 0
+    return my_list
