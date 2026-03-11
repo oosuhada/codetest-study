57f1093273a69ccad4fb87fe23b0bcb356e6d37a
우수 <woosu91@gmail.com>
2026-03-11 10:49:34 +0900

    ▶️ 코드 실행: [level 0] Title: 2차원으로 만들기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120842. 2차원으로 만들기/20260414_184123_run_2차원으로 만들기.py
@@ -0,0 +1,21 @@
+def solution(num_list, n):
+    answer = []
+    
+    #n 슬라이싱으로 깔끔한 코드 구성이 가능하지 않을지
+    #num_list[0], .. ,num_list[n-1]
+    #num_list[n], .. ,num_list[2*n-1]
+    #num_list[2n], .. ,num_list[3*n-1]
+    #len(num_list) // 2 반복
+    
+    i = 1
+    
+    if n == 2:
+        while i <= len(num_list) // 2:
+        answer += [num_list[(i-1)*n : i*n]]
+        i += 1
+    else:
+        while i < len(num_list) // 2:
+        answer += [num_list[(i-1)*n : i*n]]
+        i += 1
+    
+    return answer
