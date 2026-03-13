1223d861ed068937931eec7972e71783eeff5b0b
우수 <woosu91@gmail.com>
2026-03-13 11:27:42 +0900

    ▶️ 코드 실행: [level 0] Title: 컨트롤 제트, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/20260417_082345_run_컨트롤 제트.py
@@ -0,0 +1,30 @@
+def solution(s):
+    #Z의 index를 먼저 찾자!
+    Z_list = []
+    i = 0
+    new_s = s.split()
+    n = len(new_s)
+    
+    for i < n:
+    #while i < n:
+        if new_s[i] == "Z":
+            Z_list.append(i)
+            #i += 1
+            
+    return Z_list
+
+# 이 코드에서 무한 순환이 되었는데 왜 무한 순환이 되는지 모르겠음.
+# while i < n:
+#     if s[i] == "Z":
+#         Z_list.append(i)
+#         i += 1
+
+# 이 코드에서 왜 n값이 s의 길이만큼 제대로 안나오는지 모르겠음.. 아 리스트가 아니라 문자열이다!
+#     Z_list = []
+#     i = 0
+#     n = len(s)
+#     return n
+
+# 아래 코드로 시도 했으나 결국에는 s.split() 이었음 다시 복습
+# new_s = list(s)
+# n = len(new_s)
