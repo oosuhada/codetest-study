1ef867707708f0d2d7b16af2807d47646058ba40
우수 <woosu91@gmail.com>
2026-03-14 21:39:09 +0900

    ▶️ 코드 실행: [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/20260427_142218_run_숨어있는 숫자의 덧셈 （2）.py
@@ -0,0 +1,46 @@
+def solution(my_string):
+    my_list = []
+    for c in my_string:
+        if (int(c)).isdigit:
+            my_list.append(c)
+        else:
+            my_list.append(',')
+            
+    answer = 0
+    return my_list
+
+# 실행한 결괏값 ["a","A","b","1","B","2","c","C","3","4","o","O","p"]이 기댓값 37과 다릅니다. - 왜 replace가 안먹히는지 모르겠음
+# def solution(my_string):
+#     my_list = []
+#     for c in my_string:
+#         if c.isdigit:
+#             my_list.append(c)
+#         else:
+#             my_list.append(my_string.replace('',c))
+            
+#     answer = 0
+#     return my_list
+
+# c.isdigit: 가 인식이 안되는거 같은데.. 아 .. int를 안썼구나! 적으면서 깨달음
+# def solution(my_string):
+#     my_list = []
+#     for c in my_string:
+#         if c.isdigit:
+#             my_list.append(c)
+#         else:
+#             my_list.append('x')
+            
+#     answer = 0
+#     return my_list
+
+# ValueError: invalid literal for int() with base 10: 'a'
+# def solution(my_string):
+#     my_list = []
+#     for c in my_string:
+#         if int(c).isdigit:
+#             my_list.append(c)
+#         else:
+#             my_list.append('x')
+            
+#     answer = 0
+#     return my_list
