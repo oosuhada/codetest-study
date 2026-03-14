e047c4ace1fd582954ebdaea4ceda745dc959bf1
우수 <woosu91@gmail.com>
2026-03-14 22:36:25 +0900

    ✅ 정답: [level 0] Title: 숫자 찾기, Time: 0.00 ms, Memory: 9.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/20260421_140825_correct_숫자 찾기.py
@@ -0,0 +1,24 @@
+def solution(num, k):
+    num_string = str('s') + str(num)
+    #num_list = num_string.split()
+    a = str(k)
+    return num_string.find(a)
+
+# 전부 실행 결괏값이 -1이 나오는 이유를 모르겠음
+# def solution(num, k):
+#     num_string = str(num)
+#     #num_list = num_string.split()
+#     return num_string.find('k')
+
+# def solution(num, k):
+#     num_string = str(num)
+#     #num_list = num_string.split()
+#     a = str(k)
+#     return num_string.find('a')
+
+# 테스트 8개중 6개만 통과함
+# def solution(num, k):
+#     num_string = str(0) + str(num)
+#     #num_list = num_string.split()
+#     a = str(k)
+#     return num_string.find(a)
