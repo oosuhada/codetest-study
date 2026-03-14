1282e514f79192506aef62cc57b2ac8d6a92f1a5
우수 <woosu91@gmail.com>
2026-03-14 09:23:52 +0900

    ▶️ 코드 실행: [level 0] Title: 짝수의 합, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/20260413_161730_run_짝수의 합.py
@@ -0,0 +1,19 @@
+def solution(n):
+    answer = 0
+    
+    #1부터 n까지의 list 배열이 for c in range(n+1)가 아닌것 같음
+    for c in list.range(0,n+1):
+        if c % 2 == 0:
+            answer += c
+
+    return answer
+
+# TypeError: 'type' object is not subscriptable
+# def solution(n):
+#     answer = 0
+    
+#     for c in range[:n+1]:
+#         if c % 2 == 0:
+#             answer += c
+
+#     return answer
