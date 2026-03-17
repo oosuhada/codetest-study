9c0bdf78fa46d749c1d56ab782798d4f1b0fd480
우수 <woosu91@gmail.com>
2026-03-17 22:20:00 +0900

    ▶️ 코드 실행: [level 0] Title: 유한소수 판별하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/20260428_113253_run_유한소수 판별하기.py
@@ -0,0 +1,18 @@
+def solution(a, b):
+    new_a = 0
+    new_b = 0
+    d = 2
+    array = []
+    for c in range(b):
+        if a // c == 0 and b // c == 0 and c >1:
+            new_a = (a // c)
+            new_b = (b // c)
+
+        while d < new_b:
+            if new_b // d == 0:
+                array.append(d)
+    answer = 0
+    return array.append
+
+
+#최대 공약수 구하는 공식이 있을것 같다
