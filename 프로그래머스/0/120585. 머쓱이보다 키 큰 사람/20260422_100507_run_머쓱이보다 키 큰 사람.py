ea59c46dc94d7d2616f502f1ff4f01a7ff2603c8
우수 <woosu91@gmail.com>
2026-03-19 15:09:35 +0900

    ▶️ 코드 실행: [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/20260422_100507_run_머쓱이보다 키 큰 사람.py
@@ -0,0 +1,16 @@
+def solution(array, height):
+    new_array = array
+    new_array.append(height)
+    new_array.sort()
+    
+    answer = 0
+    return new_array
+
+# AttributeError: 'list' object has no attribute 'array'
+# def solution(array, height):
+#     new_array = array
+#     new_array.append(height)
+#     new_array2 = new_array.sort(): or new_array2 = new_array.sorted():
+    
+#     answer = 0
+#     return new_array2
