6031ddb730002b6d088177099d36c79b1467731d
우수 <woosu91@gmail.com>
2026-03-22 16:49:27 +0900

    ▶️ 코드 실행: [level 0] Title: 암호 해독, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/20260417_142100_run_암호 해독.py
@@ -0,0 +1,17 @@
+def solution(cipher, code):
+    list_cipher = list(cipher)
+    
+    answer_list = []
+    n = len(list_cipher)
+    i = 0
+    
+    #for i in list_cipher(i):
+    while i < n:
+        if (i+1) % code == 0:
+            answer_list.append(i)
+        i += 1
+
+        
+    
+    
+    return answer_list
