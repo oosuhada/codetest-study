01954c98bb17e1b612cc8627e5d84284b4befde9
우수 <woosu91@gmail.com>
2026-03-25 20:53:28 +0900

    ▶️ 코드 실행: [level 0] Title: 공 던지기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120843. 공 던지기/20260414_185109_run_공 던지기.py
@@ -0,0 +1,8 @@
+def solution(numbers, k):
+    #배열 개수가 짝수일때랑 홀수일때 계산이 다를것 같음
+    #배열을 두배로 만들면 같은 계산식으로 가능할 것 같음
+    #new_numbers = numbers*2 #가능할지
+    #print(new_numbers) #실행한 결괏값 null
+    
+    new_numbers = numbers + numbers
+    return new_numbers
