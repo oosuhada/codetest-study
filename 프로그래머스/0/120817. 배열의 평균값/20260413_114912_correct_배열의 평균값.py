ba2d525cfc446dc05b23487eee16005da258aae2
우수 <woosu91@gmail.com>
2026-03-26 20:12:37 +0900

    ✅ 정답: [level 0] Title: 배열의 평균값, Time: 0.02 ms, Memory: 9.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/20260413_114912_correct_배열의 평균값.py
@@ -0,0 +1,15 @@
+def solution(numbers):
+    answer = 0
+    total_content = 0
+    total_index = 0
+    
+    for c in numbers:
+        total_content += c
+        total_index = len(numbers)
+    answer = (total_content / total_index)
+    
+    return answer
+
+
+# def solution(numbers):
+#     return sum(numbers) / len(numbers)
