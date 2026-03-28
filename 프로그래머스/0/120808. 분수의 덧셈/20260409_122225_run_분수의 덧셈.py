6cdb1e7b2ceaef0243fa151156207c653b39d454
우수 <woosu91@gmail.com>
2026-03-28 15:39:32 +0900

    ▶️ 코드 실행: [level 0] Title: 분수의 덧셈, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/20260409_122225_run_분수의 덧셈.py
@@ -0,0 +1,31 @@
+def solution(numer1, denom1, numer2, denom2):
+
+    #answer_double = (numer1 / denom1) + (numer2 / denom2)
+    #기약 분수로 나타내는 방법 어떻게?
+    
+    [a,b] = [0,0]
+    result = [a,b]
+    
+    
+    #셋다 for로 할지 if - elif로 묶을지 고민
+    if denom1 > denom2:
+        if denom1 % denom1 == 0:
+            a += numer1 + (numer2 * (denom1/denom2))
+            b += denom1
+        else:
+            a += (numer1 * denom2) + (numer2 * denom1)
+            b += denom1 * denom2
+            
+    elif denom1 < denom2:
+        if denom2 % denom1 == 0:
+            a += (numer1 * (denom2/denom1)) + numer2
+            b += denom2
+        else:
+            a += (numer1 * denom2) + (numer2 * denom1)
+            b += denom1 * denom2
+        
+    elif denom1 == denom2:
+        a += numer1 + numer2
+        b += denom1
+        
+    return result
