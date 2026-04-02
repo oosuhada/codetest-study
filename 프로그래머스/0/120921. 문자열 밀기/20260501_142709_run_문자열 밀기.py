5a58883253d9bfa1f125c04243caf027ce334690
우수 <woosu91@gmail.com>
2026-04-02 13:27:02 +0900

    ▶️ 코드 실행: [level 0] Title: 문자열 밀기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120921. 문자열 밀기/20260501_142709_run_문자열 밀기.py
@@ -0,0 +1,9 @@
+def solution(A, B):
+    n = len(A)
+    answer = 0
+    return len(A)
+
+# hello 
+# -> ohell : A[-1] + A[:-1]
+# -> lohel : A[-2] + A[-1] + A[:-2]
+# -> llohe : A[-3] + A[-2] + A[-1] + A[:-3]
