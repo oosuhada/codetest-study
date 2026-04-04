27f711b42b6eb8b8c22c75d237e16738ad1e0bce
우수 <woosu91@gmail.com>
2026-04-04 09:40:36 +0900

    ▶️ 코드 실행: [level 0] Title: 대문자와 소문자, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/20260417_143554_run_대문자와 소문자.py
@@ -0,0 +1,32 @@
+def solution(my_string):
+    answer = ''
+    
+    for c in my_string:
+        if c.isupper == True:
+            answer += c.lower
+        else:
+            answer += c.upper
+            
+    return answer
+
+# TypeError: can only concatenate str (not "builtin_function_or_method") to str
+# def solution(my_string):
+#     answer = ''
+#     for c in my_string:
+#         if c.isupper == True:
+#             answer += c.lower
+#         else:
+#             answer += c.upper    
+#     return answer
+
+# AttributeError: 'str' object has no attribute 'append'
+# def solution(my_string):
+#     answer = ''
+    
+#     for c in my_string:
+#         if c.isupper == True:
+#             answer.append(c.lower)
+#         else:
+#             answer.append(c.upper)
+            
+#     return answer
