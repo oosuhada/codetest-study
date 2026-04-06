20f1864fa90a29f648d6957873677b24075ec66a
우수 <woosu91@gmail.com>
2026-04-06 23:10:42 +0900

    ▶️ 코드 실행: [level 0] Title: 원소들의 곱과 합, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/20260406_231040_run_원소들의 곱과 합.py
@@ -0,0 +1,18 @@
+def solution(num_list):
+    
+    product_num = 0
+    sum_num = 0
+    
+    #원소들의 곱
+    for i in num_list
+        product_num = product_num * i
+    
+    #원소들의 합
+    for i in num_list
+        sum_num += i
+    
+    if product_num < sum_num * sum_num:
+        return "1"
+    
+    if product_num > sum_num * sum_num:
+        return "0"
