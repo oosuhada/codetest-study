0c5c362d2a9dacd66bac64cb43ce1d91dd3fc87a
우수 <woosu91@gmail.com>
2026-04-06 22:33:23 +0900

    ▶️ 코드 실행: [level 0] Title: 코드 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/20260406_223321_run_코드 처리하기.py
@@ -0,0 +1,23 @@
+def solution(code):
+    #if문, 배열, code[idx] == 1이면 mode change
+    #ret = ''
+    #mode = 0이고 code[idx] == 1 아닌데 and idx 짝수면 ret += code[idx]
+    #mode = 1이고 code[idx] == 1 아닌데 idx 홀수면 ret += code[idx]
+    #idx == 0 이라면 return EMPTY 
+    
+    ref = ''
+    mode = 0 #시작할때 mode는 0임
+    
+    for idx in code 
+        if idx == 1:
+            if mode == 0:
+                mode = 1
+                if idx % 2 == 0:
+                    ref += code[idx]
+            else:
+                mode = 0
+                if 
+            
+    
+    answer = ''
+    return answer
