533682455eb6a49ae3b6d5a4336001d69d61212f
우수 <woosu91@gmail.com>
2026-04-07 18:25:30 +0900

    ▶️ 코드 실행: [level 0] Title: 수열과 구간 쿼리 3, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/20260407_182526_run_수열과 구간 쿼리 3.py
@@ -0,0 +1,10 @@
+def solution(arr, queries):
+    
+    #문제 이해
+    #arr[i], 0=<i<len(arr)
+    #queries[i,j]
+    
+    for i,j in queries(i,j):
+        arr(i) <-> arr(j) #exchange 어떤 수식으로?
+    
+    return arr
