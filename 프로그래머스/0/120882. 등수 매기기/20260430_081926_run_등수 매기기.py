58a8eb5a064bc6b132284114199910da50776046
우수 <woosu91@gmail.com>
2026-04-07 20:35:56 +0900

    ▶️ 코드 실행: [level 0] Title: 등수 매기기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120882. 등수 매기기/20260430_081926_run_등수 매기기.py
@@ -0,0 +1,12 @@
+def solution(score):
+    #영어 수학 평균점수를 먼저 계산
+    avg = []
+    for eng, math in score:
+        avg.append((eng+math)/2)
+    #리스트 내 평균 점수보다 더 큰 점수 개수 + 1
+    rank = []
+    for s in avg:
+        rank_calc = sum(1 for x in scores if x > s) + 1
+        rank.append(rank_calc)
+    answer = []
+    return rank
