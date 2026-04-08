53b209382390c6e484ccd8781e881bb0b99b9e05
우수 <woosu91@gmail.com>
2026-04-08 16:39:31 +0900

    ▶️ 코드 실행: [level 0] Title: 배열 만들기 2, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/20260408_163930_run_배열 만들기 2.py
@@ -0,0 +1,9 @@
+def solution(l, r):
+    
+    result = []
+    #5로 나눈 값이 1, 10, 11, 100, 101, 110, 111
+    for x in range[l,r+1]:
+        if included[5] and included[0]:
+            if included [2] or included [3] or included [4] or included [6] or included [7] or included [8] or included [9]
+            
+        
