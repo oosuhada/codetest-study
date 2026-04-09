a07f08fc74480e955fffa944c48267433d8c571b
우수 <woosu91@gmail.com>
2026-04-09 17:05:19 +0900

    ▶️ 코드 실행: [level 0] Title: 최빈값 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120812. 최빈값 구하기/20260409_170518_run_최빈값 구하기.py
@@ -0,0 +1,10 @@
+def solution(array):
+    counts = array.count(i) for i in array()
+    max_counts = counts.max 
+    
+    if counts.max > 1:
+        return -1
+    else:
+        return i
+    
+    return array[i] 
