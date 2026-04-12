21f9d0904d3d9e69144945e15da7ebbe56187270
우수 <woosu91@gmail.com>
2026-04-12 13:22:15 +0900

    ▶️ 코드 실행: [level 0] Title: 옹알이 (1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/20260430_082328_run_옹알이 （1）.py
@@ -0,0 +1,15 @@
+def solution(babbling):
+    for babble in babbling:
+        babble.replace(aya,"")
+    answer = 0
+    return babbling
+
+
+# def solution(babbling):
+#     for babble in babbling:
+#         babble.replace("aya","")
+#         babble.replace("ye","")
+#         babble.replace("woo","")
+#         babble.replace("ma","")
+#     answer = 0
+#     return babbling
