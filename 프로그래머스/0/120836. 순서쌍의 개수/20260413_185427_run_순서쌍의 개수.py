c7c4ec2b84bf8ba224a2819bf868c3682602b9fa
우수 <woosu91@gmail.com>
2026-04-13 18:54:29 +0900

    ▶️ 코드 실행: [level 0] Title: 순서쌍의 개수, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120836. 순서쌍의 개수/20260413_185427_run_순서쌍의 개수.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = 0
+    
+    for i>0, i<n:
+        if n % i == 0:
+            answer += 1
+            i += 1
+        else:
+            i += 1
+    
+    return answer
