68919f95e1885891880dc96f0cac4e1a38685e5c
우수 <woosu91@gmail.com>
2026-04-13 14:38:25 +0900

    ▶️ 코드 실행: [level 0] Title: 수 조작하기 2, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/20260407_181732_run_수 조작하기 2.py
@@ -0,0 +1,16 @@
+def solution(numLog):
+    
+    for i in range(len(numLog)):
+        c = numLog(i)
+    
+        if c == 1:
+            c = w
+        elif c == -1:
+            c = s
+        elif c == 10:
+            c = d
+        elif c == -10:
+            c = a
+    
+    return str(numLog)
+    
