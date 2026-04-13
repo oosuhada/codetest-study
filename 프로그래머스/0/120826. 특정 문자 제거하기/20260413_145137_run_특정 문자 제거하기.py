8d77bd106908fc0fcc08d8d86e172814fe1ee3f6
우수 <woosu91@gmail.com>
2026-04-13 14:51:39 +0900

    ▶️ 코드 실행: [level 0] Title: 특정 문자 제거하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/20260413_145137_run_특정 문자 제거하기.py
@@ -0,0 +1,13 @@
+def solution(my_string, letter):
+    answer = ''
+    list_my_string = list(my_string)
+    if letter in list_my_string:  
+        #remove는 하나만 삭제하기 때문에 replace를 써야함
+        return my_string.replace(letter,"")
+    else:
+        return my_string
+    
+#append의 반대 remove 찾아봄
+#list to string ''.join 다시 찾아봄
+
+#TypeError: can only join an iterable 오류 발생
