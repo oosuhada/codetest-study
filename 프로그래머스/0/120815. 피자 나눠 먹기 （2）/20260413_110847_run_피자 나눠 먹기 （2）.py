de31c9db5812925d46656b56ac20ac698ca89e0f
우수 <woosu91@gmail.com>
2026-04-13 11:08:50 +0900

    ▶️ 코드 실행: [level 0] Title: 피자 나눠 먹기 (2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/20260413_110847_run_피자 나눠 먹기 （2）.py
@@ -0,0 +1,13 @@
+def solution(n):
+    answer = 0
+    
+    #6과 result값의 공배수가 n으로 나눠서 0이 될때 멈춤
+    for i>0, i+
+        if i % n == 0 and i % 6 == 0:
+            answer += (i % 6)
+            return answer
+        else:
+            i+
+    
+    
+  
