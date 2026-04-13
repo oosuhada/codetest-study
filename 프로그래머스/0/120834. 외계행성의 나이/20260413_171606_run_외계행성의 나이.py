e188605035e072c2747af4c904e6d321c7d4e161
우수 <woosu91@gmail.com>
2026-04-13 17:16:08 +0900

    ▶️ 코드 실행: [level 0] Title: 외계행성의 나이, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/20260413_171606_run_외계행성의 나이.py
@@ -0,0 +1,9 @@
+def solution(age):
+    # 12를 [1,2] 배열로 바꾸는법 헷갈림. 검색해봄
+    # list(map(int, str(age)))
+    # int(''.join(map(str, age)))
+    
+    list1 = list(map(int, str(age)))
+    list1.replace(0:9,a:j)
+    return ''.join(list1)
+    
