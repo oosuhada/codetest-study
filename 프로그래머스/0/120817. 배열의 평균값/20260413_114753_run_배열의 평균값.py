b602fb68a4e2e1ab89bfbdd1994c5409139ff785
우수 <woosu91@gmail.com>
2026-04-13 11:47:54 +0900

    ▶️ 코드 실행: [level 0] Title: 배열의 평균값, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/20260413_114753_run_배열의 평균값.py
@@ -0,0 +1,12 @@
+def solution(numbers):
+    answer = 0
+    total_content = 0
+    total_index = 0
+    
+    for c in numbers:
+        total_content += c
+    for i in range(len(numbers)):
+        total_index += i
+    answer = (total_content / total_index)
+    
+    return answer
