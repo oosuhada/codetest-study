67c31f5acf3ef6ebc94d8103dd9269a93a33bc14
우수 <woosu91@gmail.com>
2026-04-13 10:09:13 +0900

    ▶️ 코드 실행: [level 0] Title: 피자 나눠 먹기 (1), Time: ., Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/20260413_100912_run_피자 나눠 먹기 （1）.py
@@ -0,0 +1,13 @@
+import math
+def solution(n):
+    
+    answer = 0
+    
+    x = n//7
+    if n % 7 == 0:
+        answer += x
+    else:
+        answer += x
+    #ceil 함수 검색해봄. 
+    
+    return answer
