d0ce555da7270aec1c0741299eb922b710b3d0b4
우수 <woosu91@gmail.com>
2026-04-13 16:13:24 +0900

    ▶️ 코드 실행: [level 0] Title: 짝수의 합, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/20260413_161323_run_짝수의 합.py
@@ -0,0 +1,18 @@
+def solution(n):
+    answer = 0
+    
+    for c in range(n+1)
+        if c % 2 == 0:
+            answer += c
+
+    return answer
+
+# TypeError: 'type' object is not subscriptable
+# def solution(n):
+#     answer = 0
+    
+#     for c in range[:n+1]:
+#         if c % 2 == 0:
+#             answer += c
+
+#     return answer
