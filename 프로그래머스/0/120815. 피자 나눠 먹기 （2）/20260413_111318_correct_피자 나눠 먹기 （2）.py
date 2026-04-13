d68fcef42f248edd5263e22e4986f6fa3e5372fe
우수 <woosu91@gmail.com>
2026-04-13 11:13:21 +0900

    ✅ 정답: [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 9.07 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/20260413_111318_correct_피자 나눠 먹기 （2）.py
@@ -0,0 +1,13 @@
+def solution(n):
+    answer = 0
+    i = 1
+    #6과 result값의 공배수가 n으로 나눠서 0이 될때 멈춤
+    while i>0: #while 대신 for문을 사용하여 틀림. 두 개념 정확히 구분 필요
+        if i % n == 0 and i % 6 == 0:
+            answer += (i // 6)
+            return answer
+        else:
+            i += 1
+    
+    
+  
