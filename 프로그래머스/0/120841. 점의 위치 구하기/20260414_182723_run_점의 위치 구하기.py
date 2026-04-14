afd52de3e7d3fa3319c5b940ef2e9697cca3ad4a
우수 <woosu91@gmail.com>
2026-04-14 18:27:24 +0900

    ▶️ 코드 실행: [level 0] Title: 점의 위치 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/20260414_182723_run_점의 위치 구하기.py
@@ -0,0 +1,12 @@
+def solution(dot):
+    for (a,b) in dot:
+        if a > 0 and b > 0:
+            return 1
+        elif a < 0 and b > 0:
+            return 2
+        elif a < 0 and b < 0:
+            return 3
+        elif a > 0 and b < 0:
+            return 4
+
+#TypeError: cannot unpack non-iterable int object
