6afd6d29a0453e96c65106df736e90392cf7843b
우수 <woosu91@gmail.com>
2026-04-14 12:54:18 +0900

    ▶️ 코드 실행: [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/20260414_125415_run_구슬을 나누는 경우의 수.py
@@ -0,0 +1,29 @@
+def solution(balls, share):
+    #경우의 수. balls 중에 share 만큼 고르는 경우의 수
+    #answer = balls! / ((balls - share)! * share!)
+    
+    #balls!
+    a = balls
+    aa = 1
+    while a >= 1:
+        aa *= a
+        a -= 1 
+        
+    #share!
+    b = share
+    bb = 1
+    while b >= 1:
+        bb *= b
+        b -= 1
+        
+    #(balls - share)!
+    if balls > share :
+        c = (balls - share)
+        cc = 1
+        while c >= 1:
+            cc *= c
+            c -= 1
+    elif balls == share:
+        cc = 1
+        
+    return ac / (bb*cc)
