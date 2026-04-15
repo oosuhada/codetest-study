53122dc934c1d3f453af30b23c72bcca91dcf1e7
우수 <woosu91@gmail.com>
2026-04-15 13:42:42 +0900

    ▶️ 코드 실행: [level 0] Title: 대문자와 소문자, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/20260417_152031_run_대문자와 소문자.py
@@ -0,0 +1,56 @@
+def solution(my_string):
+    answer = ''
+    my_string2 = my_string
+    
+    for c in my_string2:
+        if c.isupper == False:
+            my_string2.replace(c, c.upper()) 
+        else:
+            my_string2.replace(c, c.lower()) 
+            
+    return my_string2
+
+# TypeError: can only concatenate str (not "builtin_function_or_method") to str
+# def solution(my_string):
+#     answer = ''
+#     for c in my_string:
+#         if c.isupper == True:
+#             answer += c.lower
+#         else:
+#             answer += c.upper    
+#     return answer
+
+# AttributeError: 'str' object has no attribute 'append'
+# def solution(my_string):
+#     answer = ''
+    
+#     for c in my_string:
+#         if c.isupper == True:
+#             answer.append(c.lower)
+#         else:
+#             answer.append(c.upper)
+            
+#     return answer
+
+# NameError: name 'upper' is not defined
+# def solution(my_string):
+#     answer = ''
+    
+#     for c in my_string:
+#         if c.isupper == True:
+#             answer += lower(c)
+#         else:
+#             answer += upper(c)
+#     return answer
+
+# 실행한 결괏값 "cccCCC"이 기댓값 "CCCccc"과 다릅니다.
+# def solution(my_string):
+#     answer = ''
+    
+#     for c in my_string:
+#         if c.isupper == False:
+#             c.replace(c, c.upper()) 
+#         else:
+#             c.replace(c, c.lower()) 
+            
+#     return my_string
