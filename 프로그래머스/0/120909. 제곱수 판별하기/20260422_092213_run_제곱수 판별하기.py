941c7d63ce35d964f8475259e5d318ce8a2911a0
우수 <woosu91@gmail.com>
2026-04-16 10:33:02 +0900

    ▶️ 코드 실행: [level 0] Title: 제곱수 판별하기, Time: ., Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/20260422_092213_run_제곱수 판별하기.py
@@ -0,0 +1,16 @@
+def solution(n):
+    for x in range(0,1001):
+        if (x * x) == n:
+            return 1
+        else:
+            return 2
+
+        
+# 입력값 〉	144
+# 실행 결과 〉	실행한 결괏값 2이 기댓값 1과 다릅니다.       
+# def solution(n):
+#     for x in range(1,1001):
+#         if x * x == n:
+#             return 1
+#         else:
+#             return 2
