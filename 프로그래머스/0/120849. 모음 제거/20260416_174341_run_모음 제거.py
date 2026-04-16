6706f04fd17129df4ed0365745a0d45b17723525
우수 <woosu91@gmail.com>
2026-04-16 17:43:42 +0900

    ▶️ 코드 실행: [level 0] Title: 모음 제거, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/20260416_174341_run_모음 제거.py
@@ -0,0 +1,9 @@
+def solution(my_string):
+    answer = my_string
+    #a, e, i, o, u
+    answer = string.replace("a",'')
+    answer = string.replace("e",'')
+    answer = string.replace("i",'')
+    answer = string.replace("o",'')
+    answer = string.replace("u",'')
+    return answer
