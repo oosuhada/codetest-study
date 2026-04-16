252ab390a3f8416fd08c15cb6f4a4e4c32fecb39
우수 <woosu91@gmail.com>
2026-04-17 08:14:00 +0900

    ▶️ 코드 실행: [level 0] Title: 컨트롤 제트, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/20260417_081359_run_컨트롤 제트.py
@@ -0,0 +1,14 @@
+def solution(s):
+    #Z의 index를 먼저 찾자!
+    Z_list = []
+    i = 0
+    n = len(s)
+    
+    #for i in s[i]:
+    while i < n:
+        if s[i] == "Z":
+            Z_list.append(i)
+    
+            
+    answer = 0
+    return Z_list
