39ec9ff8927e9a6430e826a3584a0e16f375b9e6
우수 <woosu91@gmail.com>
2026-04-17 15:41:14 +0900

    ▶️ 코드 실행: [level 0] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/20260417_154113_run_인덱스 바꾸기.py
@@ -0,0 +1,27 @@
+def solution(my_string, num1, num2):
+    answer = ''
+    
+    my_list = list(my_string)
+    new_list = []
+    new_list += my_list
+    
+    new_list[num1] = my_list[num2]
+    
+    new_list2 = new_list
+    new_list2[num2] = my_list[num1]
+    
+    return new_list2
+
+
+# 입력값 〉	"hello", 1, 2
+# 기댓값 〉	"hlelo"
+# 실행 결과 〉	실행한 결괏값 ["h","l","l","l","o"]이 기댓값 "hlelo"과 다릅니다.
+# def solution(my_string, num1, num2):
+#     answer = ''
+    
+#     my_list = list(my_string)
+#     new_list = my_list
+#     new_list[num1] = my_list[num2]
+#     new_list[num2] = my_list[num1]
+    
+#     return new_list
