5da7044a36da0dacc3d6c1bc68f768421659d27f
우수 <woosu91@gmail.com>
2026-04-17 12:38:34 +0900

    ▶️ 코드 실행: [level 0] Title: 가까운 수, Time: ., Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/0/120890. 가까운 수/20260417_123833_run_가까운 수.py
@@ -0,0 +1,12 @@
+def solution(array, n):
+    
+    new_array = array
+    new_array.append(n)
+    new_array.sort()
+    
+    for i in range(0,len(new_array)):
+        if new_array[i] == n:
+            if (n - new_array[i-1]) > (new_array[i+1]-n):
+                return new_array[i+1]
+            elif (n - new_array[i-1]) <= (new_array[i+1]-n):
+                return new_array[i-1]
