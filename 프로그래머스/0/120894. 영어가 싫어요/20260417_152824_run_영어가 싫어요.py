7d4f84c937a5e27615318d3b1facb7d5d4ce251a
우수 <woosu91@gmail.com>
2026-04-17 15:28:25 +0900

    ▶️ 코드 실행: [level 0] Title: 영어가 싫어요, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/20260417_152824_run_영어가 싫어요.py
@@ -0,0 +1,13 @@
+def solution(numbers):
+    answer0 = numbers.replace("zero","0")
+    answer1 = numbers0.replace("one","1")
+    answer2 = numbers1.replace("two","2")
+    answer3 = numbers2.replace("three","3")
+    answer4 = numbers3.replace("four","4")
+    answer5 = numbers4.replace("five","5")
+    answer6 = numbers5.replace("six","6")
+    answer7 = numbers6.replace("seven","7")
+    answer8 = numbers7.replace("eight","8")
+    answer9 = numbers8.replace("nine","9")
+    
+    return int(answer9)
