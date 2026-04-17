2103b4ed115545ac1c5a6870256845206f071d4c
우수 <woosu91@gmail.com>
2026-04-17 16:01:36 +0900

    ▶️ 코드 실행: [level 0] Title: 한 번만 등장한 문자, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/20260417_160135_run_한 번만 등장한 문자.py
@@ -0,0 +1,24 @@
+def solution(s):
+    temp_list = []
+    for c in s:
+        temp_list.append(c)
+    
+    for a,b in temp_list:
+        if a == b:
+            temp_list.replace('a','')
+            temp_list.replace('b','')
+
+    return temp_list
+
+
+# ValueError: not enough values to unpack (expected 2, got 1)
+# def solution(s):
+#     temp_list = []
+#     for c in s:
+#         temp_list.append(c)
+    
+#     for a,b in temp_list:
+#         if a == b:
+#             temp_list.remove(a,b)
+#     answer = ''
+#     return temp_list
