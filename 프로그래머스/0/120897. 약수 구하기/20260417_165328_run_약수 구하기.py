4c3fe85fa443e5f3781075667f03469e4929f834
우수 <woosu91@gmail.com>
2026-04-17 16:53:29 +0900

    ▶️ 코드 실행: [level 0] Title: 약수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/20260417_165328_run_약수 구하기.py
@@ -0,0 +1,16 @@
+def solution(n):
+    answer = []
+    
+    for c in range(1:n+1):
+        if n % c == 0:
+            answer.append(c)
+            
+    return answer
+
+# TypeError: 'type' object is not subscriptable
+# def solution(n):
+#     answer = []
+#     for c in range[0:n+1]:
+#         if n % c == 0:
+#             answer.append(c)
+#     return answer
