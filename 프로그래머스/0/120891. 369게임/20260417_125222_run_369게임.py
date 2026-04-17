5fad87cf87fb49d7f48ee34c0ed0df5fcc70074a
우수 <woosu91@gmail.com>
2026-04-17 12:52:23 +0900

    ▶️ 코드 실행: [level 0] Title: 369게임, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/20260417_125222_run_369게임.py
@@ -0,0 +1,7 @@
+def solution(order):
+    order2 = str(order)
+    order3 = order2.replace('3','a')
+    order3 = order3.replace('6','a')
+    order3 = order3.replace('9','a')
+    answer = 0
+    return order3
