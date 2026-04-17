820a23a6814619b54e377b468e326b90a521c42a
우수 <woosu91@gmail.com>
2026-04-17 12:18:26 +0900

    ▶️ 코드 실행: [level 0] Title: 중복된 문자 제거, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/20260417_121825_run_중복된 문자 제거.py
@@ -0,0 +1,27 @@
+def solution(my_string):
+    #중복인지를 판별하는게 중요할듯 -> set 사용?
+
+    temp_list = list(my_string)
+    answer_list = []
+    
+    for c in temp_list:
+        if c not in answer_list:
+            answer_list.append(c)
+    
+    return answer_list
+
+
+#["e","o","p","l"] 이렇게 set으로 순서가 바뀌어버림
+# temp_list = list(my_string)
+# temp_list2 = list(set(temp_list))
+
+# answer = ''
+# return temp_list2
+
+# TypeError: 'builtin_function_or_method' object is not subscriptable
+# temp_list = list(my_string)
+# answer_list = []
+# for c in temp_list:
+#     if c not in answer_list:
+#         answer_list.append[c]
+# return answer_list
