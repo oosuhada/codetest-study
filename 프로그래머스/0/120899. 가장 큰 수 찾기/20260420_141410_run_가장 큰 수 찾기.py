cdd8b1f4239b36c7b4de4520123498d167c31db5
우수 <woosu91@gmail.com>
2026-04-20 14:14:12 +0900

    ▶️ 코드 실행: [level 0] Title: 가장 큰 수 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/20260420_141410_run_가장 큰 수 찾기.py
@@ -0,0 +1,9 @@
+def solution(array):
+    answer = []
+    answer.append(max(array))
+    
+    for i in array(i):
+        if max(array) == array(i):
+            answer.append(i)
+            
+    return answer
