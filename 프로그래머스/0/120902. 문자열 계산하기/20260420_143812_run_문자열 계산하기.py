6903062039333225c2695e2b7dfa3bad4a1d4fca
우수 <woosu91@gmail.com>
2026-04-20 14:38:13 +0900

    ▶️ 코드 실행: [level 0] Title: 문자열 계산하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/20260420_143812_run_문자열 계산하기.py
@@ -0,0 +1,15 @@
+def solution(my_string):
+    my_list = ''.join(my_string)
+    a = my_string.index(+)
+    b = my_list.index(-)
+    return a
+# def solution(my_string):
+#     answer = ','.join(my_string)
+#     a = int(answer[0])
+#     c = answer[4]
+#     b = int(answer[8])
+    
+#     if c == '+':
+#         return a + b
+#     else:
+#         return a - b
