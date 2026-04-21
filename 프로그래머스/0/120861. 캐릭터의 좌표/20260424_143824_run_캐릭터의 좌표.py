be548ea5510247837ef8aff37bca08feebf7d655
우수 <woosu91@gmail.com>
2026-04-21 19:41:58 +0900

    ▶️ 코드 실행: [level 0] Title: 캐릭터의 좌표, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120861. 캐릭터의 좌표/20260424_143824_run_캐릭터의 좌표.py
@@ -0,0 +1,14 @@
+def solution(keyinput, board):
+    answer = [a, b]
+    d, e = board
+    for c in keyinput:
+        while a <= d and b <= e:
+            if c == "left":
+                a -= 1
+            elif c == "right":
+                a += 1
+            elif c == "down":
+                b -= 1
+            elif c == "up":
+                b += 1
+        return answer
