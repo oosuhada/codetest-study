63d12c004fd20027766be147d1471b28611f4305
우수 <woosu91@gmail.com>
2026-04-21 14:06:27 +0900

    ▶️ 코드 실행: [level 0] Title: 숫자 찾기, Time: ., Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/20260421_140626_run_숫자 찾기.py
@@ -0,0 +1,11 @@
+def solution(num, k):
+    num_string = str(num)
+    #num_list = num_string.split()
+    a = str(k)
+    return num_string.find(a)
+
+# 전부 실행 결괏값이 -1이 나오는 이유를 모르겠음
+# def solution(num, k):
+#     num_string = str(num)
+#     #num_list = num_string.split()
+#     return num_string.find('k')
