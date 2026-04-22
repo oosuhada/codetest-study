c5ccf140be13f14ac8d54711adbd80310f9ac27f
우수 <woosu91@gmail.com>
2026-04-22 09:50:45 +0900

    ▶️ 코드 실행: [level 0] Title: 잘라서 배열로 저장하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/20260422_095044_run_잘라서 배열로 저장하기.py
@@ -0,0 +1,10 @@
+def solution(my_str, n):
+    leng = len(my_str)
+    x = leng // str(n)
+    answer = []
+    
+    for i in range(1,x):
+        answer.append(my_str[0,i*int(n)+1])
+    answer.append(my_str[x*int(n)+1:leng+1])
+    
+    return answer
