b80cdb017b09afab6a6f0ca0c27e2676c584e49e
우수 <woosu91@gmail.com>
2026-04-23 22:28:16 +0900

    ▶️ 코드 실행: [level 0] Title: 최댓값 만들기 (1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/20260416_171050_run_최댓값 만들기 （1）.py
@@ -0,0 +1,7 @@
+def solution(numbers):
+    n = len(numbers)
+    #작은수부터 큰 수까지 정렬하는 방법 까먹어서 arrage(numbers), array(numbers) 
+    #시도하였으나 NameError: name 'array' is not defined
+    numbers.arrange
+    answer = numbers[n] * numbers[n-1]
+    return answer
