cd5ac40543dbdaa3376e3b69282381a9999e4696
우수 <woosu91@gmail.com>
2026-04-24 14:29:25 +0900

    ❌ 오답: [level 0] Title: 직사각형 넓이 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/20260424_142924_wrong_직사각형 넓이 구하기.py
@@ -0,0 +1,62 @@
+def solution(dots):
+    a, b, c, d = dots
+    x1, y1 = a
+    x2, y2 = b
+    x3, y3 = c
+    x4, y4 = d
+    
+    width = abs(x1 - x2)
+    height = abs(y3 - y2)
+    
+    return (width*height)
+
+# ValueError: not enough values to unpack (expected 4, got 2)
+# def solution(dots):
+#     for [[x1, y1], [x2, y2], [x3, y3], [x4, y4]] in dots:
+#         answer = [x1, y1]
+#     return answer
+
+# def solution(dots):
+#     for [a,b,c,d] in dots:
+#         answer = a
+#     return answer
+
+# python array unpack 방법 검색함
+# a, b, c, d = dots
+
+# python definite difference between two minus numbers 검색함
+# diff = abs(x - y)
+
+# 테스트 1 〉	실패 (0.00ms, 9.25MB)
+# 테스트 2 〉	실패 (0.00ms, 9.01MB)
+# 테스트 3 〉	실패 (0.00ms, 9.25MB)
+# 테스트 4 〉	실패 (0.00ms, 9.27MB)
+# 테스트 5 〉	실패 (0.00ms, 9.2MB)
+# def solution(dots):
+#     a, b, c, d = dots
+#     x1, y1 = a
+#     x2, y2 = b
+#     x3, y3 = c
+#     x4, y4 = d
+    
+#     width = abs(x2 - x1)
+#     height = abs(y2 - y3)
+    
+#     return width * height
+
+# 테스트 1 〉	실패 (0.00ms, 9.25MB)
+# 테스트 2 〉	실패 (0.00ms, 9.27MB)
+# 테스트 3 〉	실패 (0.00ms, 9.16MB)
+# 테스트 4 〉	실패 (0.00ms, 9.36MB)
+# 테스트 5 〉	실패 (0.00ms, 9.12MB)
+# def solution(dots):
+#     a, b, c, d = dots
+#     x1, y1 = a
+#     x2, y2 = b
+#     x3, y3 = c
+#     x4, y4 = d
+    
+#     width = abs(x1 - x2)
+#     height = abs(y3 - y2)
+    
+#     return width * height
