c42a8d5dd1c0c911025ad034830da1065e127a00
우수 <woosu91@gmail.com>
2026-04-24 14:20:52 +0900

    ▶️ 코드 실행: [level 0] Title: 직사각형 넓이 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/20260424_142050_run_직사각형 넓이 구하기.py
@@ -0,0 +1,14 @@
+def solution(dots):
+    a, b, c, d = dots
+    return a
+
+# ValueError: not enough values to unpack (expected 4, got 2)
+# def solution(dots):
+#     for [[x1, y1], [x2, y2], [x3, y3], [x4, y4]] in dots:
+#         answer = [x1, y1]
+#     return answer
+
+# def solution(dots):
+#     for [a,b,c,d] in dots:
+#         answer = a
+#     return answer
