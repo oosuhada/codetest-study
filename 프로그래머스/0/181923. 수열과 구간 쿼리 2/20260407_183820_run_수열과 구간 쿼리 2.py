2915eac4e928412fa2676f96fd3d553c6d40703d
우수 <woosu91@gmail.com>
2026-04-25 16:26:37 +0900

    ▶️ 코드 실행: [level 0] Title: 수열과 구간 쿼리 2, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/20260407_183820_run_수열과 구간 쿼리 2.py
@@ -0,0 +1,12 @@
+def solution(arr, queries):
+    
+    answer = []
+    for s, e, k in queries:
+        for i in arr:
+            if s <= i <= e:
+                if arr[i] >= k:
+                    return answer += arr[i].min
+                else:
+                    return answer += "-1"
+                    
+    return answer
