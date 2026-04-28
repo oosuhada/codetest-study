e3cb6c5d88c147d9b1404ef63f6d57248cb99c63
우수 <woosu91@gmail.com>
2026-04-28 11:36:30 +0900

    ▶️ 코드 실행: [level 0] Title: 유한소수 판별하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/20260428_113629_run_유한소수 판별하기.py
@@ -0,0 +1,51 @@
+def solution(a, b):
+    new_a = 0
+    new_b = 0
+    d = 2
+    array = []
+    
+    for c in range(2,b+1):
+        if a // c == 0 and b // c == 0 and c >1:
+            new_a = (a / c)
+            new_b = (b / c)
+
+    answer = 0
+    return [new_a, new_b]
+
+
+#최대 공약수 구하는 공식이 있을것 같다
+
+# ZeroDivisionError: integer division or modulo by zero
+# def solution(a, b):
+#     new_a = 0
+#     new_b = 0
+#     d = 2
+#     array = []
+#     for c in range(b):
+#         if a // c == 0 and b // c == 0 and c >1:
+#             new_a = (a // c)
+#             new_b = (b // c)
+
+#         while d < new_b:
+#             if new_b // d == 0:
+#                 array.append(d)
+#     answer = 0
+#     return array.append
+
+# TypeError: Object of type builtin_function_or_method is not JSON serializable
+# def solution(a, b):
+#     new_a = 0
+#     new_b = 0
+#     d = 2
+#     array = []
+    
+#     for c in range(2,b+1):
+#         if a // c == 0 and b // c == 0 and c >1:
+#             new_a = (a // c)
+#             new_b = (b // c)
+
+#         while d < new_b:
+#             if new_b // d == 0:
+#                 array.append(d)
+#     answer = 0
+#     return array.append
