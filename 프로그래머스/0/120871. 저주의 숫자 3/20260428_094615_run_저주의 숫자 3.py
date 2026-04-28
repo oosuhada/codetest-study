d3298a81fbba6e2c715561f8b714ea0c8935f144
우수 <woosu91@gmail.com>
2026-04-28 09:46:16 +0900

    ▶️ 코드 실행: [level 0] Title: 저주의 숫자 3, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120871. 저주의 숫자 3/20260428_094615_run_저주의 숫자 3.py
@@ -0,0 +1,18 @@
+def solution(n):
+    x = n
+    answer = n
+    
+    for i in range(n):
+        if i % 3 == 0:
+            answer += 1
+    for k in range(n):
+        if str(k).contains('3'):
+            answer += 1
+    
+    return answer
+
+
+# 3x 마을에서 쓰는 숫자 x까지의 정수 배열 - 3의 배수 개수 - 3이 포함된 숫자 개수 + (3의 배수이면서 3이 포함된 숫자 개수) = 10진법 숫자 n
+
+# 3x 마을에서 쓰는 숫자 x = 10진법 숫자 n + 3의 배수 개수 + 3이 포함된 숫자 개수 -  (3의 배수이면서 3이 포함된 숫자 개수)
+
