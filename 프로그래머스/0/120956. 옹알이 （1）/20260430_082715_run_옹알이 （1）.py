4a7302725f0e872c12575d837462a468d3f0f49a
우수 <woosu91@gmail.com>
2026-04-30 08:27:17 +0900

    ▶️ 코드 실행: [level 0] Title: 옹알이 (1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/20260430_082715_run_옹알이 （1）.py
@@ -0,0 +1,33 @@
+def solution(babbling):
+    babbling2 = babbling[:]
+    new = []
+    for babble in babbling2:
+        babble2 = babble
+        new.append(babble2.replace('aya',''))
+    answer = 0
+    return new
+
+
+# def solution(babbling):
+#     for babble in babbling:
+#         babble.replace("aya","")
+#         babble.replace("ye","")
+#         babble.replace("woo","")
+#         babble.replace("ma","")
+#     answer = 0
+#     return babbling
+
+# 실행한 결괏값 ["aya","yee","u","maa","wyeoo"]이 기댓값 1과 다릅니다.
+# aya 가 왜 repalce 되지 않았는지 이해하지 못함
+# def solution(babbling):
+#     for babble in babbling:
+#         babble.replace('aya','')
+#     answer = 0
+#     return babbling
+
+# def solution(babbling):
+#     babbling2 = babbling[:]
+#     for babble in babbling2:
+#         babble.replace('aya','')
+#     answer = 0
+#     return babbling2
