6686011730e7bd965e2335b9ed66516fe3b23494
우수 <woosu91@gmail.com>
2026-05-01 12:32:45 +0900

    ▶️ 코드 실행: [level 0] Title: 치킨 쿠폰, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/20260501_123243_run_치킨 쿠폰.py
@@ -0,0 +1,13 @@
+def solution(chicken):
+    order = (chicken // 10) + (chicken // 10 + chicken % 10) // 100
+    return order
+
+
+# 입력값 〉	1081
+# 기댓값 〉	120
+# 실행 결과 〉	실행한 결괏값 119이 기댓값 120과 다릅니다.
+# def solution(chicken):
+#     order = (chicken // 10) + (chicken // 100) + (chicken //
+# 1000) + (chicken // 10000) ...
+
+# 수식을 어떻게 짜야할지 모르겠음
