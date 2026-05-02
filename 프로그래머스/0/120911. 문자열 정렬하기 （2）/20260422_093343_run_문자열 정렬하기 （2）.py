d0987e0290ce3fa6c2692d1cd3c27f6bcc377012
우수 <woosu91@gmail.com>
2026-05-02 20:26:44 +0900

    ▶️ 코드 실행: [level 0] Title: 문자열 정렬하기 (2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/20260422_093343_run_문자열 정렬하기 （2）.py
@@ -0,0 +1,20 @@
+def solution(my_string):
+    answer = ''
+    for x in my_string:
+        if x.isupper:
+            answer.append(x.lower)
+        else:
+            answer.append(x)
+    # answer = answer.sort()
+    return answer
+
+# TypeError: can only concatenate str (not "builtin_function_or_method") to str
+# def solution(my_string):
+#     answer = ''
+#     for x in my_string:
+#         if x.isupper:
+#             answer += x.lower
+#         else:
+#             answer += x
+#     answer = answer.sort()
+#     return answer
