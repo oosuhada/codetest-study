0d12f568a8e8954aea91cc8aaa4abee5b4fa6ac5
우수 <woosu91@gmail.com>
2026-05-04 10:22:57 +0900

    ▶️ 코드 실행: [level 0] Title: 주사위 게임 3, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/181916. 주사위 게임 3/20260504_102256_run_주사위 게임 3.py
@@ -0,0 +1,11 @@
+def solution(a, b, c, d):
+    list_1 = [a, b, c, d]
+    a1, b1, c1, d1 = list_1.sorted()
+    answer = 0
+    return list_1
+
+# AttributeError: 'list' object has no attribute 'sorted'
+# def solution(a, b, c, d):
+#     list = [a, b, c, d]
+#     a1, b1, c1, d1 = list.sorted()
+#     return list
