329d1a071ca2492dcc9f254d856d4c7c97099733
우수 <woosu91@gmail.com>
2026-05-11 15:19:05 +0900

    ▶️ 코드 실행: [level 0] Title: 최댓값 만들기 (2), Time: ., Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/20260424_145314_run_최댓값 만들기 （2）.py
@@ -0,0 +1,22 @@
+def solution(numbers):
+    leng = len(numbers)
+    new_list = []
+    
+    for c in numbers:
+        new_list.append(abs(c))
+    new_list.sort()
+    numbers.sort()
+
+    return  max(numbers[leng-1] * numbers[leng-2], numbers[0] * numbers[1])
+
+
+# 마이너스 값은 마이너스 값이어야 하는데 절대값으로 생각함
+# def solution(numbers):
+#     leng = len(numbers)
+#     new_list = []
+    
+#     for c in numbers:
+#         new_list.append(abs(c))
+#     new_list.sort()
+    
+#     return new_list[leng-2] * new_list[leng-1]
