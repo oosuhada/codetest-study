de90e0fccd2a21c0261930e0fcd5e880caff3fc7
우수 <woosu91@gmail.com>
2026-05-11 09:12:18 +0900

    ▶️ 코드 실행: [level 0] Title: 문자열 정렬하기 (1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/20260416_180516_run_문자열 정렬하기 （1）.py
@@ -0,0 +1,35 @@
+def solution(my_string):
+    #직전에 모음만 replace하는 방식을 다뤄서인지 replace밖에 생각이 나지 않는데
+    #dictionary 방식이라던지 더 효율적인 방식이 있을것 같음
+    
+    new_string = my_string
+    new_string = new_string.replace("a",'')
+    new_string = new_string.replace("b",'')
+    new_string = new_string.replace("c",'')
+    new_string = new_string.replace("d",'')
+    new_string = new_string.replace("e",'')
+    new_string = new_string.replace("f",'')
+    new_string = new_string.replace("g",'')
+    new_string = new_string.replace("h",'')
+    new_string = new_string.replace("i",'')
+    new_string = new_string.replace("j",'')
+    new_string = new_string.replace("k",'')
+    new_string = new_string.replace("l",'')
+    new_string = new_string.replace("m",'')
+    new_string = new_string.replace("n",'')
+    new_string = new_string.replace("o",'')
+    new_string = new_string.replace("p",'')
+    new_string = new_string.replace("q",'')
+    new_string = new_string.replace("r",'')
+    new_string = new_string.replace("s",'')
+    new_string = new_string.replace("t",'')
+    new_string = new_string.replace("u",'')
+    new_string = new_string.replace("v",'')
+    new_string = new_string.replace("w",'')
+    new_string = new_string.replace("x",'')
+    new_string = new_string.replace("y",'')
+    new_string = new_string.replace("z",'')
+    new_list = list(new_string)
+    new_list.sort()
+
+    return new_list
