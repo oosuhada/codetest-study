84e7120b2b8e40e353ec0f6d84f9bb8fd5774d77
우수 <woosu91@gmail.com>
2026-05-11 09:08:29 +0900

    ▶️ 코드 실행: [level 0] Title: 평행, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/20260428_103442_run_평행.py
@@ -0,0 +1,7 @@
+def solution(dots):
+    [[x1, y1], [x2, y2], [x3, y3], [x4, y4]] = dots
+    return x1
+
+# #평행인지 검증하는 코드 어떻게 구성할 것인가
+# ValueError: not enough values to unpack (expected 4, got 2)
+# for [a,b],[c,d],[e,f],[g,h] in dots:
