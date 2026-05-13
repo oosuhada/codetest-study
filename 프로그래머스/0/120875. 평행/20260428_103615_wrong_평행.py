43929e1294741f9c1be75911eb96a5918ac9afe3
우수 <woosu91@gmail.com>
2026-05-13 22:22:51 +0900

    ❌ 오답: [level 0] Title: 평행, Time: 0.00 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/20260428_103615_wrong_평행.py
@@ -0,0 +1,11 @@
+def solution(dots):
+    [[x1, y1], [x2, y2], [x3, y3], [x4, y4]] = dots
+    
+    if (x1-x2) == (x3-x4) and (y1-y2) == (y3-y4):
+        return 1
+    else:
+        return 0
+
+# #평행인지 검증하는 코드 어떻게 구성할 것인가
+# ValueError: not enough values to unpack (expected 4, got 2)
+# for [a,b],[c,d],[e,f],[g,h] in dots:
