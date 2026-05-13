627179b6546a2424d42f0ea2000e241975049920
우수 <woosu91@gmail.com>
2026-05-13 09:44:27 +0900

    ▶️ 코드 실행: [level 0] Title: 한 번만 등장한 문자, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/20260417_155437_run_한 번만 등장한 문자.py
@@ -0,0 +1,10 @@
+def solution(s):
+    temp_list = []
+    for c in s:
+        temp_list.append(c)
+    
+    for a,b in temp_list:
+        if a == b:
+            temp_list.remove(a,b)
+    answer = ''
+    return temp_list
