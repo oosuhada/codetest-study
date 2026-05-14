424e7aad4b62d55cedeaa79d4c52add57ddc9eba
우수 <woosu91@gmail.com>
2026-05-14 20:42:07 +0900

    ▶️ 코드 실행: [level 0] Title: 다항식 더하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120863. 다항식 더하기/20260424_145920_run_다항식 더하기.py
@@ -0,0 +1,13 @@
+def solution(polynomial):
+    element = polynomial.split(" + ")
+    a = []
+    b = []
+    
+    for c in element:
+        if c.contains('x'):
+            a.append(c)
+        elif
+            b.append(c)
+            
+    answer = ''
+    return [a,b]
