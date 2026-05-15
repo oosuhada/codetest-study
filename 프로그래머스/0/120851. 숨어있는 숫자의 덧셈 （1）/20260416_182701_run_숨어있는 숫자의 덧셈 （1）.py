1ec73aca9440ebf339bf2538edcb62b5f69f7d21
우수 <woosu91@gmail.com>
2026-05-15 13:39:46 +0900

    ▶️ 코드 실행: [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/20260416_182701_run_숨어있는 숫자의 덧셈 （1）.py
@@ -0,0 +1,14 @@
+def solution(my_string):
+    
+    my_list = []
+    #직전 문자열 정렬하기 에서 .isdigit 함수 새로 익히고 활용
+    for c in my_string:
+        if c.isdigit():
+            my_list.append(c)
+    
+    answer = 0
+    for c2 in my_list():
+        answer += int(c2)
+        
+    return answer
+
