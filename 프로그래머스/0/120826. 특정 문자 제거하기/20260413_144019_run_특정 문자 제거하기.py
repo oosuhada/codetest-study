b1de7a41495b53359f6faf1f2a84cd4295f8fd26
우수 <woosu91@gmail.com>
2026-05-20 19:30:12 +0900

    ▶️ 코드 실행: [level 0] Title: 특정 문자 제거하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/20260413_144019_run_특정 문자 제거하기.py
@@ -0,0 +1,7 @@
+def solution(my_string, letter):
+    answer = ''
+    list_my_string = list(my_string)
+    if letter in list_my_string:
+        return list_my_string.append(-1, letter)
+    else:
+        return my_string
