f1f86720e65dc9a5a191ffbe709d83e349d85ab4
우수 <woosu91@gmail.com>
2026-05-21 20:07:17 +0900

    ▶️ 코드 실행: [level 0] Title: 특정 문자 제거하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/20260413_144130_run_특정 문자 제거하기.py
@@ -0,0 +1,9 @@
+def solution(my_string, letter):
+    answer = ''
+    list_my_string = list(my_string)
+    if letter in list_my_string:
+        return ''.list_my_string.remove(letter)
+    else:
+        return my_string
+    
+#append의 반대 remove 찾아봄
