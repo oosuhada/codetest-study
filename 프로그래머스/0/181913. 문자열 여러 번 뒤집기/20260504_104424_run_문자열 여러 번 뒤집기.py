fe5cba1ca6cdb0c6305efc06530fa28d92945c00
우수 <woosu91@gmail.com>
2026-05-21 22:26:01 +0900

    ▶️ 코드 실행: [level 0] Title: 문자열 여러 번 뒤집기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/20260504_104424_run_문자열 여러 번 뒤집기.py
@@ -0,0 +1,7 @@
+def solution(my_string, queries):
+    answer = ''
+    for row in queries:
+        s, e = row
+        answer = my_string[0:s] + my_string[e:s] + my_string[e:]
+
+    return my_string
