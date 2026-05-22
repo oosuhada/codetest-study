3920d67821e1ef1e30c16363ee44db91051544ec
우수 <woosu91@gmail.com>
2026-05-22 16:12:48 +0900

    ▶️ 코드 실행: [level 0] Title: 콜라츠 수열 만들기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/20260408_171508_run_콜라츠 수열 만들기.py
@@ -0,0 +1,15 @@
+def solution(n):
+    
+    result = []
+    x = n
+    
+    for x>1
+        if x == 1:
+            return result
+        else:
+                if x % 2 == 0:
+                x = x/2
+                result.append(x)
+                else:
+                x = (3 * x) + 1
+                result.append(x)
