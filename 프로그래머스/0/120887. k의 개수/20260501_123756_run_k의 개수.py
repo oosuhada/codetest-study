4a4fbbd05278dfd16ffff04f1160df0eeb5cd520
우수 <woosu91@gmail.com>
2026-05-24 11:05:12 +0900

    ▶️ 코드 실행: [level 0] Title: k의 개수, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120887. k의 개수/20260501_123756_run_k의 개수.py
@@ -0,0 +1,14 @@
+def solution(i, j, k):
+    answer = ''
+    for x in range(i,j+1):
+        answer += str(x)
+
+    return answer.count(str(k))
+
+# TypeError: can only concatenate str (not "int") to str
+# def solution(i, j, k):
+#     answer = ''
+#     for x in range(i,j+1):
+#         answer += x
+
+#     return answer
