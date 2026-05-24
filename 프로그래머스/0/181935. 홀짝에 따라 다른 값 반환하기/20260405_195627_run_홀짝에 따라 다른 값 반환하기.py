59c655ca22157b14b9372ec312b30d2ac3db0d48
우수 <woosu91@gmail.com>
2026-05-24 14:30:28 +0900

    ▶️ 코드 실행: [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/20260405_195627_run_홀짝에 따라 다른 값 반환하기.py
@@ -0,0 +1,14 @@
+def solution(n):
+    
+    answer = 0
+    
+    # 1부터 n까지 반복
+    for i in range(1, n+1):
+        
+    if n % 2 == 1:
+        if i % 2 == 1:
+            answer += i
+    else i % 2 == 0:
+        answer += i*i
+        
+    return answer
