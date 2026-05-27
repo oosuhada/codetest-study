888e143d9aa1d9365c9745937641e96890576c1c
우수 <woosu91@gmail.com>
2026-05-27 15:27:06 +0900

    ▶️ 코드 실행: [level 0] Title: 겹치는 선분의 길이, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/20260428_105938_run_겹치는 선분의 길이.py
@@ -0,0 +1,7 @@
+def solution(lines):
+    [[a1, b1], [a2, b2], [a3, b3]] = lines
+    start = min(a1,a2,a3,b1,b2,b3)
+    end = max(a1,a2,a3,b1,b2,b3)
+    array = list(range(min,max+1))
+    answer = 0
+    return array
