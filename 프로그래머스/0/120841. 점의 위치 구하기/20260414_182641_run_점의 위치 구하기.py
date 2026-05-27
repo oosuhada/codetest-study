4c5c005857208acdb3d572703d8b431f242e5a9b
우수 <woosu91@gmail.com>
2026-05-27 11:46:43 +0900

    ▶️ 코드 실행: [level 0] Title: 점의 위치 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/20260414_182641_run_점의 위치 구하기.py
@@ -0,0 +1,11 @@
+def solution(dot):
+    for [a,b] in dot:
+        if a > 0 and b > 0:
+            return 1
+        elif a < 0 and b > 0:
+            return 2
+        elif a < 0 and b < 0:
+            return 3
+        elif a > 0 and b < 0:
+            return 4
+        
