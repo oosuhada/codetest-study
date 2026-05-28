3495badbcc5b53086d5c90058293185d501df252
우수 <woosu91@gmail.com>
2026-05-28 15:48:54 +0900

    ▶️ 코드 실행: [level 0] Title: 컨트롤 제트, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/20260417_081453_run_컨트롤 제트.py
@@ -0,0 +1,21 @@
+def solution(s):
+    #Z의 index를 먼저 찾자!
+    Z_list = []
+    i = 0
+    n = len(s)
+    
+    #for i in s[i]:
+    while i < n:
+        if s[i] == "Z":
+            Z_list.append(i)
+            i += 1
+    
+            
+    answer = 0
+    return n
+
+    # 이 코드에서 무한 순환이 되었는데 왜 무한 순환이 되는지 모르겠음.
+    # while i < n:
+    #     if s[i] == "Z":
+    #         Z_list.append(i)
+    #         i += 1
