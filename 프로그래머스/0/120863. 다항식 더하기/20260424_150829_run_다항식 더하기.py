86abdb37947b6ea1f2f1a50bf6e71d99a99fa460
우수 <woosu91@gmail.com>
2026-05-28 21:41:06 +0900

    ▶️ 코드 실행: [level 0] Title: 다항식 더하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120863. 다항식 더하기/20260424_150829_run_다항식 더하기.py
@@ -0,0 +1,50 @@
+def solution(polynomial):
+    element = polynomial.split(" + ")
+    a = []
+    b = []
+    
+    for c in element:
+        if 'x' in c:
+            a.append(c)
+        else:
+            b.append(c)
+    
+    d = 0
+    e = 0
+    for x in a:
+        if x == 'x':
+            d += 1
+        else:
+            d += int(x.replace('x',''))
+    
+    for y in b:
+        e += int(y)
+        
+    return str(d)+'x + '+str(e)
+
+# AttributeError: 'str' object has no attribute 'has'
+# def solution(polynomial):
+#     element = polynomial.split(" + ")
+#     a = []
+#     b = []
+    
+#     for c in element:
+#         if c.contains('x'):
+#             a.append(c)
+#         else:
+#             b.append(c)
+            
+#     answer = ''
+#     return [a,b]
+
+# AttributeError: 'str' object has no attribute 'remove'
+#    d = 0
+#     e = 0
+#     for x in a:
+#         if x == 'x':
+#             d += 1
+#         else:
+#             d += int(x.remove('x'))
+        
+#     answer = ''
+#     return [d,e]
