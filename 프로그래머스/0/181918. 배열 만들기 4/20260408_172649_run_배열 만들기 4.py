2fd4d11e656aa6edbf23f2a8a85fed58d3d7b592
우수 <woosu91@gmail.com>
2026-05-29 13:07:59 +0900

    ▶️ 코드 실행: [level 0] Title: 배열 만들기 4, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/20260408_172649_run_배열 만들기 4.py
@@ -0,0 +1,14 @@
+def solution(arr):
+    stk = []
+    i = 0
+    
+    while len(stk) > len(arr):
+        if len(stk) == 0:
+            stk.append(arr[i])
+        elif stk[-1] < arr[i]:
+            stk.append(arr[i])
+            i += 1
+        elif stk[-1] >= arr[i]:
+            stk.append(-1)
+    
+    return stk
