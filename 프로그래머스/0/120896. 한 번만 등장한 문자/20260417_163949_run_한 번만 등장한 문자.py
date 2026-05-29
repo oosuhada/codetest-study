05df8fb761e8de31c1cfd4deb736ff9bd8cce93a
우수 <woosu91@gmail.com>
2026-05-29 16:17:35 +0900

    ▶️ 코드 실행: [level 0] Title: 한 번만 등장한 문자, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/20260417_163949_run_한 번만 등장한 문자.py
@@ -0,0 +1,59 @@
+def solution(s):
+    answer = ''
+    for c in s:
+        s.count(c) == 1 :
+            answer += c
+
+# ValueError: not enough values to unpack (expected 2, got 1)
+# def solution(s):
+#     temp_list = []
+#     for c in s:
+#         temp_list.append(c)
+    
+#     for a,b in temp_list:
+#         if a == b:
+#             temp_list.remove(a,b)
+#     answer = ''
+#     return temp_list
+
+# ValueError: not enough values to unpack (expected 2, got 1)
+# def solution(s):
+#     temp_list = []
+#     for c in s:
+#         temp_list.append(c)
+    
+#     for a,b in temp_list:
+#         if a == b:
+#             temp_list.replace('a','')
+#             temp_list.replace('b','')
+
+#     return temp_list
+
+
+
+# def solution(s):
+#     temp_list = []
+#     for c in s:
+#         temp_list.append(c)
+#     answer = ''
+    
+#     for a in temp_list
+#     if temp_list.count(a) > 1:
+#         temp_list.replace(a,'')
+    
+#     return temp_list
+
+# 입력값 〉	"abcabcadc"
+# 기댓값 〉	"d"
+# 실행 결과 〉	실행한 결괏값 "ababad"이 기댓값 "d"과 다릅니다.
+# def solution(s):
+#     temp_list = []
+#     for c in s:
+#         temp_list.append(c)
+#     answer = ''
+    
+#     for a in s:
+#         if s.count('a') > 1:
+#             temp_list = s.replace(a,'')
+    
+#     return temp_list
